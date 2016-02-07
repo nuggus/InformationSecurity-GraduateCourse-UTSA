732c7b614e40a603a927816c8c37ef4115e6e452
nuggus <nugguviswanathcse@gmail.com>
2016-02-07 15:08:54 -0600

    Delete BookServerInterface.java

--- Java RMI Project/BookServerInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface BookServerInterface extends Remote {
-	BookServiceAttribute search(String topic) throws RemoteException;//for search
-	BookServiceAttribute bookDetails(String bookId) throws RemoteException;//for lookup
-	BookServiceAttribute orderBook(String bookId,int no) throws RemoteException;//for order
-	BookServiceAttribute reports() throws RemoteException;//for reports
-}
